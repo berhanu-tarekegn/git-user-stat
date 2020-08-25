ae60d01ae67b59beee3e5bac3e3f6d9a227f91a5
Berhanu <btarekegn@apposit.com>
2020-08-25 08:36:59 +0300

    Initial commit, GithubInput and GithubOutput components

---
+++ src/App/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+
+  <p>Hello World</p>
+
+  <github-input></github-input>
+
+</div>
+</template>
+
+<script src="./script.js"></script>
+<style scoped src="./style.css"></style>
